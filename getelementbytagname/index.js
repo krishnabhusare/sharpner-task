a169ab7aa2f48a9cc6f493cc14b4c39e33d1a0eb
krishnabhusare <krishnabhusare1996@gmail.com>
2024-05-13 01:08:14 +0530

    getelementbytagname

---
+++ getelementbytagname/index.js
@@ -0,0 +1,13 @@
+/*const listitems= document.getElementsByTagName("li");
+listitems[2].style.color="pink";
+for(let i=0;i<listitems.length;i++){
+    listitems[i].style.fontStyle="italic";
+} */
+
+const listitems=document.getElementsByTagName("li");
+listitems[4].style.color="blue";
+
+for(let i=0;i<listitems.length;i++)
+{
+    listitems[i].style.fontStyle="italic";
+}
