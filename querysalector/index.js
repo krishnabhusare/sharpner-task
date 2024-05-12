a36d348b1508092499d0acd4189621b03ced4093
krishnabhusare <krishnabhusare1996@gmail.com>
2024-05-13 01:10:57 +0530

    querysalector

---
+++ querysalector/index.js
@@ -0,0 +1,47 @@
+/*  
+const mainheading=document.querySelector("#main-heading");
+mainheading.style.textAlign="center";
+
+const fruits=document.querySelector(".fruits");
+fruits.style.backgroundColor="gray";
+fruits.style.padding="30px";
+fruits.style.margin="30px";
+fruits.style.width="50%";
+fruits.style.borderRadius="5px";
+fruits.style.listStyleType="none";
+
+const bascketheading=document.querySelector("h2");
+bascketheading.style.marginLeft="30px";
+
+const fruititems=document.querySelectorAll(".fruit");
+for(let i=0;i<fruititems.length;i++)
+{
+    fruititems[i].style.backgroundColor="white";
+    
+    fruititems[i].style.padding="10px";
+    fruititems[i].style.margin="10px";
+   
+    fruititems[i].style.borderRadius="5px";
+    
+}
+
+const oddfruititems=document.querySelectorAll(".fruit:nth-child(odd)");
+for(let i=0;i<oddfruititems.length;i++)
+{
+    oddfruititems[i].style.backgroundColor="lightgray";
+}  */
+
+const basketheading=document.querySelector("#basket-heading");
+basketheading.style.color="brown";
+
+
+
+const fruititems=document.querySelectorAll(".fruit:nth-child(even");
+for(let i=0;i<fruititems.length;i++)
+{
+    fruititems[i].style.backgroundColor="brown";
+    fruititems[i].style.color="white";
+}
+
+
+
