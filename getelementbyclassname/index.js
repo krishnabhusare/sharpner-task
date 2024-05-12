8199166d71915533ad5858ddc47e91a93f75476e
krishnabhusare <krishnabhusare1996@gmail.com>
2024-05-13 01:05:35 +0530

    getelementbyclassname

---
+++ getelementbyclassname/index.js
@@ -0,0 +1,14 @@
+/*const fruit=document.getElementsByClassName("fruit");
+fruit[1].textContent="pinaple";
+fruit[1].style.fontWeight="bold";
+for(let i=0;i<fruit.length;i++){
+    fruit[i].style.backgroundColor="gray";
+
+}*/
+const fruit =document.getElementsByClassName("fruit");
+fruit[2].style.backgroundColor="yellow";
+
+for(let i=0;i<fruit.length;i++)
+{
+   fruit[i].style.fontWeight="bold";
+}
